7b40176dfa00ee679a64ac0f5e7cb12a6462f49e
Han Xiao <han.xiao@cs.helsinki.fi>
2015-11-05 20:30:52 +0200

    transformed enron dataset

--- meta_graph.py
+++ meta_graph.py
@@ -11,35 +11,35 @@ from collections import defaultdict
 from util import load_items_by_line
 
 def convert_to_meta_graph(node_names, sources, targets, time_stamps):
-        """
-        sources: list of source node id for each interaction
-        targets: list of target node ids for each interaction
-        time_stamps: happening time of the interactions
+    """
+    sources: list of source node id for each interaction
+    targets: list of target node ids for each interaction
+    time_stamps: happening time of the interactions
 
-        All four fields shall be sorted from earliest to lastest
-        according to time_stamps
-        """
-        assert len(node_names) == len(sources) == len(targets) == len(time_stamps)
-        g = nt.DiGraph()
-        
-        s2n = defaultdict(set)  # source to nodes mapping
-        for n, s in zip(node_names, sources):
-            s2n[s].add(n)
-        
-        for n1, s, ts in zip(node_names, sources, targets):
-            # remove entries of n1 in s2n
-            s2n[s].remove(n1)
+    All four fields shall be sorted from earliest to lastest
+    according to time_stamps
+    """
+    assert len(node_names) == len(sources) == len(targets) == len(time_stamps)
+    g = nt.DiGraph()
+    
+    s2n = defaultdict(set)  # source to nodes mapping
+    for n, s in zip(node_names, sources):
+        s2n[s].add(n)
+    
+    for n1, s, ts in zip(node_names, sources, targets):
+        # remove entries of n1 in s2n
+        s2n[s].remove(n1)
 
-            # add node, can be singleton
-            g.add_node(n1)
-            
-            # add edges
-            for n2 in s2n[s]:
+        # add node, can be singleton
+        g.add_node(n1)
+        
+        # add edges
+        for n2 in s2n[s]:
+            g.add_edge(n1, n2)
+        for t in ts:
+            for n2 in s2n[t]:
                 g.add_edge(n1, n2)
-            for t in ts:
-                for n2 in s2n[t]:
-                    g.add_edge(n1, n2)
-        return g
+    return g
 
 
 class EnronUtil(object):
