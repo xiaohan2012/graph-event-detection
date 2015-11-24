0801e234a4c61cd3977a34f95ca719e138363461
Han Xiao <han.xiao@cs.helsinki.fi>
2015-11-24 16:09:04 +0200

    added vertex reward attribute

--- lst.py
+++ lst.py
@@ -4,7 +4,10 @@ from networkx.classes.digraph import DiGraph
 from networkx.algorithms.dag import topological_sort
 
 
-def lst_dag(G, r, U, debug=False):
+def lst_dag(G, r, U,
+            node_reward_key='r',
+            edge_cost_key='c',
+            debug=False):
     """
     Param:
     -------------
@@ -20,7 +23,7 @@ def lst_dag(G, r, U, debug=False):
     A, D, BP = {}, {}, {}
     for n in ns:
         A[n] = {}  # maximum sum of node u at a cost i
-        A[n][0] = 1
+        A[n][0] = G.node[n][node_reward_key]
 
         D[n] = {}  # set of nodes included corresponding to A[u][i]
         D[n][0] = {n}
@@ -29,19 +32,20 @@ def lst_dag(G, r, U, debug=False):
 
     for n in topological_sort(G, reverse=True):  # leaves come first
         children = G.neighbors(n)
+        reward = G.node[n][node_reward_key]
         if len(children) == 1:
             child = children[0]
-            w = G[n][child]['w']
+            w = G[n][child][edge_cost_key]
             for i in xrange(U, w - 1, -1):
                 if (i-w) in A[child]:
-                    A[n][i] = A[child][i-w] + 1
+                    A[n][i] = A[child][i-w] + reward
                     D[n][i] = D[child][i-w] | {n}
                     BP[n][i] = [(child, i-w)]
         elif len(children) > 1:
             assert len(children) == 2
             lchild, rchild = children
-            lw = G[n][lchild]['w']
-            rw = G[n][rchild]['w']
+            lw = G[n][lchild][edge_cost_key]
+            rw = G[n][rchild][edge_cost_key]
 
             for i in xrange(U, min(lw, rw) - 1, -1):
                 max_val = float('-inf')
@@ -49,20 +53,20 @@ def lst_dag(G, r, U, debug=False):
                 prev = None
 
                 if i - lw >= 0 and (i - lw) in A[lchild]:
-                    max_val = A[lchild][i - lw] + 1
+                    max_val = A[lchild][i - lw] + reward
                     max_nodes = D[lchild][i - lw] | {n}
                     prev = [(lchild, i - lw)]
                 
                 if (i - rw >= 0
                     and (i - rw) in A[rchild]
-                    and A[rchild][i - rw] + 1 > max_val):
-                    max_val = A[rchild][i - rw] + 1
+                    and A[rchild][i - rw] + reward > max_val):
+                    max_val = A[rchild][i - rw] + reward
                     max_nodes = D[rchild][i - rw] | {n}
                     prev = [(rchild, i - rw)]
 
                 for j in xrange(i - lw - rw, -1, -1):
                     if j in A[lchild] and (i-j-lw-rw) in A[rchild]:
-                        val = A[lchild][j] + A[rchild][i-j-lw-rw] + 1
+                        val = A[lchild][j] + A[rchild][i-j-lw-rw] + reward
                         lset, rset = D[lchild][j], D[rchild][i-j-lw-rw]
                         if val > max_val and len(lset & rset) == 0:
                             max_val = val
@@ -88,87 +92,87 @@ def lst_dag(G, r, U, debug=False):
     return tree
 
 
-def nw_lst_dag(G, r, U, debug=False):
-    """
-    length-constrained maximum-sum tree for DAG with nw(node weights)
-    Param:
-    -------------
-    binary_dag: a DAG in networkx format. Each node can have at most 2 child
-    r: root node in dag
-    U: the maximum threshold of edge weight sum
+# def nw_lst_dag(G, r, U, debug=False):
+#     """
+#     length-constrained maximum-sum tree for DAG with nw(node weights)
+#     Param:
+#     -------------
+#     binary_dag: a DAG in networkx format. Each node can have at most 2 child
+#     r: root node in dag
+#     U: the maximum threshold of edge weight sum
 
-    Return:
-    maximum-sum subtree rooted at r whose sum of edge weights <= A
-    ------------
-    """
-    ns = G.nodes()
-    A, D, BP = {}, {}, {}
-    for n in ns:
-        A[n] = {}  # maximum sum of node u at a cost i
-        D[n] = {}  # set of nodes included corresponding to A[u][i]
+#     Return:
+#     maximum-sum subtree rooted at r whose sum of edge weights <= A
+#     ------------
+#     """
+#     ns = G.nodes()
+#     A, D, BP = {}, {}, {}
+#     for n in ns:
+#         A[n] = {}  # maximum sum of node u at a cost i
+#         D[n] = {}  # set of nodes included corresponding to A[u][i]
 
-        w = G.node[n]['w']
-        if w <= U:
-            A[n][w] = 1
-            D[n][w] = {n}
+#         w = G.node[n]['w']
+#         if w <= U:
+#             A[n][w] = 1
+#             D[n][w] = {n}
 
-        BP[n] = defaultdict(list)  # backpointer corresponding to A[u][i]
+#         BP[n] = defaultdict(list)  # backpointer corresponding to A[u][i]
 
-    for n in topological_sort(G, reverse=True):  # leaves come first
-        children = G.neighbors(n)  # children
-        if len(children) == 1:
-            child = children[0]
-            w = G.node[n]['w' ]
-            for i in xrange(U, w - 1, -1):
-                if (i-w) in A[child]:
-                    A[n][i] = A[child][i-w] + 1
-                    D[n][i] = D[child][i-w] | {n}
-                    BP[n][i] = [(child, i-w)]
-        elif len(children) > 1:
-            assert len(children) == 2
-            lchild, rchild = children
-            w = G.node[n]['w']
+#     for n in topological_sort(G, reverse=True):  # leaves come first
+#         children = G.neighbors(n)  # children
+#         if len(children) == 1:
+#             child = children[0]
+#             w = G.node[n]['w' ]
+#             for i in xrange(U, w - 1, -1):
+#                 if (i-w) in A[child]:
+#                     A[n][i] = A[child][i-w] + 1
+#                     D[n][i] = D[child][i-w] | {n}
+#                     BP[n][i] = [(child, i-w)]
+#         elif len(children) > 1:
+#             assert len(children) == 2
+#             lchild, rchild = children
+#             w = G.node[n]['w']
 
-            for i in xrange(U, w - 1, -1):
-                max_val = float('-inf')
-                max_nodes = None
-                prev = None
+#             for i in xrange(U, w - 1, -1):
+#                 max_val = float('-inf')
+#                 max_nodes = None
+#                 prev = None
 
-                if i - w >= 0 and (i - w) in A[lchild]:
-                    max_val = A[lchild][i - w] + 1
-                    max_nodes = D[lchild][i - w] | {n}
-                    prev = [(lchild, i - w)]
+#                 if i - w >= 0 and (i - w) in A[lchild]:
+#                     max_val = A[lchild][i - w] + 1
+#                     max_nodes = D[lchild][i - w] | {n}
+#                     prev = [(lchild, i - w)]
                 
-                if (i - w >= 0
-                    and (i - w) in A[rchild]
-                    and A[rchild][i - w] + 1 > max_val):
-                    max_val = A[rchild][i - w] + 1
-                    max_nodes = D[rchild][i - w] | {n}
-                    prev = [(rchild, i - w)]
+#                 if (i - w >= 0
+#                     and (i - w) in A[rchild]
+#                     and A[rchild][i - w] + 1 > max_val):
+#                     max_val = A[rchild][i - w] + 1
+#                     max_nodes = D[rchild][i - w] | {n}
+#                     prev = [(rchild, i - w)]
 
-                for j in xrange(i - w, -1, -1):
-                    if j in A[lchild] and (i-j-w) in A[rchild]:
-                        val = A[lchild][j] + A[rchild][i-j-w] + 1
-                        lset, rset = D[lchild][j], D[rchild][i-j-w]
-                        if val > max_val and len(lset & rset) == 0:
-                            max_val = val
-                            max_nodes = lset | rset | {n}
-                            prev = [(lchild, j), (rchild, i-j-w)]
-                A[n][i] = max_val
-                D[n][i] = max_nodes
-                BP[n][i] = prev
-            if n == r:  # no need to continue once we processed root
-                break
+#                 for j in xrange(i - w, -1, -1):
+#                     if j in A[lchild] and (i-j-w) in A[rchild]:
+#                         val = A[lchild][j] + A[rchild][i-j-w] + 1
+#                         lset, rset = D[lchild][j], D[rchild][i-j-w]
+#                         if val > max_val and len(lset & rset) == 0:
+#                             max_val = val
+#                             max_nodes = lset | rset | {n}
+#                             prev = [(lchild, j), (rchild, i-j-w)]
+#                 A[n][i] = max_val
+#                 D[n][i] = max_nodes
+#                 BP[n][i] = prev
+#             if n == r:  # no need to continue once we processed root
+#                 break
 
-    best_cost = max(xrange(U + 1),
-                    key=lambda i: A[r][i] if i in A[r] else float('-inf'))
-    tree = DiGraph()
-    stack = []
-    for n, cost in BP[r][best_cost]:
-        stack.append((r, n, cost))
-    while len(stack) > 0:
-        parent, child, cost = stack.pop(0)
-        tree.add_edge(parent, child)
-        for grandchild, cost2 in BP[child][cost]:
-            stack.append((child, grandchild, cost2))
-    return tree
+#     best_cost = max(xrange(U + 1),
+#                     key=lambda i: A[r][i] if i in A[r] else float('-inf'))
+#     tree = DiGraph()
+#     stack = []
+#     for n, cost in BP[r][best_cost]:
+#         stack.append((r, n, cost))
+#     while len(stack) > 0:
+#         parent, child, cost = stack.pop(0)
+#         tree.add_edge(parent, child)
+#         for grandchild, cost2 in BP[child][cost]:
+#             stack.append((child, grandchild, cost2))
+#     return tree
--- test_lst_dag.py
+++ test_lst_dag.py
@@ -1,16 +1,22 @@
-from .lst import lst_dag, nw_lst_dag
+from .lst import lst_dag
 from nose.tools import assert_equal
 from networkx.classes.digraph import DiGraph
 
 
 def test_lst_dag():
     # input
-    dag = DiGraph()
-    dag.add_edges_from([[1, 2], [1, 3], [2, 4], [3, 4]])
-    dag[1][2]['w'] = 2
-    dag[1][3]['w'] = 1
-    dag[2][4]['w'] = 1
-    dag[3][4]['w'] = 3
+    g = DiGraph()
+    g.add_edges_from([[1, 2], [1, 3], [2, 4], [3, 4]])
+
+    g.node[1]['r'] = 1
+    g.node[2]['r'] = 1
+    g.node[3]['r'] = 1.5
+    g.node[4]['r'] = 1
+
+    g[1][2]['c'] = 2
+    g[1][3]['c'] = 1
+    g[2][4]['c'] = 1
+    g[3][4]['c'] = 3
 
     r = 1  # root
     U = range(6)  # 0...5
@@ -22,45 +28,45 @@ def test_lst_dag():
         one_node_tree,
         DiGraph([(1, 3)]),
         DiGraph([(1, 3)]),  # DiGraph([(1, 2)])
-        DiGraph([(1, 2), (2, 4)]),  # DiGraph([(1, 2), (1, 3)])
+        DiGraph([(1, 2), (1, 3)]),
         DiGraph([(1, 2), (1, 3), (2, 4)]),
         DiGraph([(1, 2), (1, 3), (2, 4)])
     ]
 
     for u, expected in zip(U, expected_dags):
-        actual = lst_dag(dag, r, u)
-        print(actual.edges())
-        assert_equal(actual.edges(), expected.edges())
+        actual = lst_dag(g, r, u)
+        print('u={}'.format(u))
+        assert_equal(expected.edges(), actual.edges())
 
 
-def test_nw_lst_dag():
-    # input
-    dag = DiGraph()
-    dag.add_edges_from([[1, 2], [1, 3], [2, 4], [3, 4]])
-    dag.node[1]['w'] = 1
-    dag.node[2]['w'] = 2
-    dag.node[3]['w'] = 1
-    dag.node[4]['w'] = 1
+# def test_nw_lst_dag():
+#     # input
+#     g = DiGraph()
+#     g.add_edges_from([[1, 2], [1, 3], [2, 4], [3, 4]])
+#     g.node[1]['w'] = 1
+#     g.node[2]['w'] = 2
+#     g.node[3]['w'] = 1
+#     g.node[4]['w'] = 1
     
-    r = 1  # root
-    U = range(7)  # 0...6
+#     r = 1  # root
+#     U = range(7)  # 0...6
 
-    # expected value
-    empty_tree = DiGraph()
-    one_node_tree = DiGraph()
-    one_node_tree.add_node(1)
-    expected_dags = [
-        empty_tree,
-        one_node_tree,
-        DiGraph([(1, 3)]),
-        DiGraph([(1, 3), (3, 4)]),
-        DiGraph([(1, 3), (3, 4)]),  # DiGraph([(1, 2), (2, 3)])
-        DiGraph([(1, 2), (1, 3), (2, 4)]),  # DiGraph([(1, 2), (1, 3), (3, 4)])
-        DiGraph([(1, 2), (1, 3), (2, 4)])  # DiGraph([(1, 2), (1, 3), (3, 4)])
-    ]
+#     # expected value
+#     empty_tree = DiGraph()
+#     one_node_tree = DiGraph()
+#     one_node_tree.add_node(1)
+#     expected_dags = [
+#         empty_tree,
+#         one_node_tree,
+#         DiGraph([(1, 3)]),
+#         DiGraph([(1, 3), (3, 4)]),
+#         DiGraph([(1, 3), (3, 4)]),  # DiGraph([(1, 2), (2, 3)])
+#         DiGraph([(1, 2), (1, 3), (2, 4)]),  # DiGraph([(1, 2), (1, 3), (3, 4)])
+#         DiGraph([(1, 2), (1, 3), (2, 4)])  # DiGraph([(1, 2), (1, 3), (3, 4)])
+#     ]
 
-    for u, expected in zip(U, expected_dags):
-        print('u', u)
-        actual = nw_lst_dag(dag, r, u)
-        print(actual.edges())
-        assert_equal(actual.edges(), expected.edges())
+#     for u, expected in zip(U, expected_dags):
+#         print('u', u)
+#         actual = nw_lst_dag(g, r, u)
+#         print(actual.edges())
+#         assert_equal(actual.edges(), expected.edges())
