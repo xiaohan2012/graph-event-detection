5597934c1173ca96e2e19320e7a468f7d68bfc74
Han Xiao <han.xiao@cs.helsinki.fi>
2015-11-24 16:09:40 +0200

    removed nw_lst_dag

--- test_lst_dag.py
+++ test_lst_dag.py
@@ -37,36 +37,3 @@ def test_lst_dag():
         actual = lst_dag(g, r, u)
         print('u={}'.format(u))
         assert_equal(expected.edges(), actual.edges())
-
-
-# def test_nw_lst_dag():
-#     # input
-#     g = DiGraph()
-#     g.add_edges_from([[1, 2], [1, 3], [2, 4], [3, 4]])
-#     g.node[1]['w'] = 1
-#     g.node[2]['w'] = 2
-#     g.node[3]['w'] = 1
-#     g.node[4]['w'] = 1
-    
-#     r = 1  # root
-#     U = range(7)  # 0...6
-
-#     # expected value
-#     empty_tree = DiGraph()
-#     one_node_tree = DiGraph()
-#     one_node_tree.add_node(1)
-#     expected_dags = [
-#         empty_tree,
-#         one_node_tree,
-#         DiGraph([(1, 3)]),
-#         DiGraph([(1, 3), (3, 4)]),
-#         DiGraph([(1, 3), (3, 4)]),  # DiGraph([(1, 2), (2, 3)])
-#         DiGraph([(1, 2), (1, 3), (2, 4)]),  # DiGraph([(1, 2), (1, 3), (3, 4)])
-#         DiGraph([(1, 2), (1, 3), (2, 4)])  # DiGraph([(1, 2), (1, 3), (3, 4)])
-#     ]
-
-#     for u, expected in zip(U, expected_dags):
-#         print('u', u)
-#         actual = nw_lst_dag(g, r, u)
-#         print(actual.edges())
-#         assert_equal(actual.edges(), expected.edges())
