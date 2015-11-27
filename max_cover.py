b3da967d856fa9eaa2678c520fc4ffc29599401c
Han Xiao <han.xiao@cs.helsinki.fi>
2015-11-27 13:56:43 +0200

    argmax_k_coverage added

--- max_cover.py
+++ max_cover.py
@@ -1,4 +1,5 @@
 # Algorithm for maximum coverage problem
+import numpy as np
 
 
 def maximum_k_coverage(sets, k):
@@ -12,4 +13,19 @@ def maximum_k_coverage(sets, k):
         selected_sets.append(max_set)
         covered |= max_set
     return selected_sets
+
+
+def argmax_k_coverage(sets, k):
+    """return the indices of the selected sets
+    """
+    covered = set()
+    selected_set_indices = []
+    if k >= len(sets):
+        return range(len(sets))
         
+    for i in xrange(k):
+        uncovered_sizes = map(lambda s: len(s - covered), sets)
+        max_set_index = np.argmax(uncovered_sizes)
+        selected_set_indices.append(max_set_index)
+        covered |= sets[max_set_index]
+    return selected_set_indices
--- test_max_cover.py
+++ test_max_cover.py
@@ -1,5 +1,5 @@
 import unittest
-from .max_cover import maximum_k_coverage
+from .max_cover import maximum_k_coverage, argmax_k_coverage
 from nose.tools import assert_equal
 
 
@@ -47,4 +47,50 @@ class MaximumKCoverageTest(unittest.TestCase):
         assert_equal(
             example,
             maximum_k_coverage(example, 100)
+        )
+
+
+class ArgmaxKCoverageTest(unittest.TestCase):
+    def test_case_independent_sets(self):
+        example = [
+            set([1, 2, 3]),
+            set([4, 5]),
+            set([6])
+        ]
+        
+        assert_equal(
+            [0, 1],
+            argmax_k_coverage(example, 2)
+        )
+
+    def test_case_overlapping_sets(self):
+        example = [
+            set([1, 2, 3]),
+            set([2, 3, 4]),
+            set([4, 5, 6]),
+            set([6])
+        ]
+        
+        assert_equal(
+            [0, 2],
+            argmax_k_coverage(example, 2)
+        )
+        
+    def test_empty_sets(self):
+        example = []
+        assert_equal(
+            [],
+            argmax_k_coverage(example, 1)
+        )
+
+    def test_large_k(self):
+        example = [
+            set([1, 2, 3]),
+            set([2, 3, 4]),
+            set([4, 5, 6]),
+            set([6])
+        ]
+        assert_equal(
+            [0, 1, 2, 3],
+            argmax_k_coverage(example, 100)
         )
