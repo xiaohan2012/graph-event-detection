7f308697ea874504054f11f73f82a3ef2b2d2938
Han Xiao <han.xiao@cs.helsinki.fi>
2015-12-08 17:44:21 +0200

    mod gen_experiment_cmds.sh

--- scripts/gen_experiment_cmds.sh
+++ scripts/gen_experiment_cmds.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
 methods=("lst" "greedy" "random")
-dist_funcs=("entropy" "euclidean" "cosine")
+dist_funcs=("euclidean" "cosine")
 
 for method in "${methods[@]}"
 do    
   for dist_func in "${dist_funcs[@]}"
   do
-      echo "python gen_candidate_trees.py ${method} ${dist_func}"
+      echo "python gen_candidate_trees.py --method ${method} --dist ${dist_func}"
   done
+done
+
+for dist_func in "${dist_funcs[@]}"
+do
+    echo "python gen_candidate_trees.py --method lst --dist ${dist_func} --dij"
 done
