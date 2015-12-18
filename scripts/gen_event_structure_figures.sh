e62291359091c400f6677bf2e163dcf88e412f52
Han Xiao <han.xiao@cs.helsinki.fi>
2015-12-18 12:44:05 +0200

    updated figure generation script

--- scripts/gen_event_structure_figures.sh
+++ scripts/gen_event_structure_figures.sh
@@ -1,7 +1,7 @@
-paths=("result-greedy--U=0.5--dijkstra=False--timespan=28days----dist_func=cosine.pkl"	"result-greedy--U=0.5--dijkstra=False--timespan=28days----dist_func=euclidean.pkl"	"result-lst--U=0.5--dijkstra=False--timespan=28days----dist_func=cosine.pkl"	"result-lst--U=0.5--dijkstra=False--timespan=28days----dist_func=euclidean.pkl"	"result-lst--U=0.5--dijkstra=True--timespan=28days----dist_func=cosine.pkl"	"result-lst--U=0.5--dijkstra=True--timespan=28days----dist_func=euclidean.pkl"	"result-random--U=0.5--dijkstra=False--timespan=28days----dist_func=cosine.pkl"	"result-random--U=0.5--dijkstra=False--timespan=28days----dist_func=euclidean.pkl")
+paths=("result-greedy--U=0.05--dijkstra=False--timespan=56days----decompose_interactions=False--dist_func=cosine.pkl"	"result-greedy--U=0.05--dijkstra=False--timespan=56days----decompose_interactions=False--dist_func=euclidean.pkl"	"result-lst--U=0.05--dijkstra=False--timespan=56days----decompose_interactions=False--dist_func=cosine.pkl"	"result-lst--U=0.05--dijkstra=False--timespan=56days----decompose_interactions=False--dist_func=euclidean.pkl"	"result-lst--U=0.05--dijkstra=True--timespan=56days----decompose_interactions=False--dist_func=cosine.pkl"	"result-lst--U=0.05--dijkstra=True--timespan=56days----decompose_interactions=False--dist_func=euclidean.pkl"	"result-random--U=0.05--dijkstra=False--timespan=56days----decompose_interactions=False--dist_func=cosine.pkl"	"result-random--U=0.05--dijkstra=False--timespan=56days----decompose_interactions=False--dist_func=euclidean.pkl")
 
 for p in "${paths[@]}"
 do
-	echo $p
-	python event_graph_structure.py tmp/$p
+    echo $p
+    python event_graph_structure.py tmp/$p
 done
