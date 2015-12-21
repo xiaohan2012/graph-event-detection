e8f569eb755fb065ed240d0a089768ecf8ac54e7
Han Xiao <han.xiao@cs.helsinki.fi>
2015-12-22 00:25:45 +0200

    more parameter combination in experiment

--- scripts/gen_meta_graphs.sh
+++ scripts/gen_meta_graphs.sh
@@ -2,5 +2,13 @@
 
 common="--lda=models/model-25-100.lda --weeks=8 --res_dir=tmp/lda-25-topics"
 
-python gen_candidate_trees.py --dist cosine --calc_mg --cand_n 0 --method random ${common}
-python gen_candidate_trees.py --dist euclidean --calc_mg --cand_n 0 --method random  ${common}
+dist_funcs=("cosine" "euclidean")
+
+for dist_func in "${dist_funcs[@]}"
+do
+    base="python gen_candidate_trees.py --dist ${dist_func} --calc_mg --cand_n 0 --method random ${common}"
+    eval $base
+    eval "${base} --decompose"
+done
+
+# python gen_candidate_trees.py --dist euclidean --calc_mg --cand_n 0 --method random  ${common}
