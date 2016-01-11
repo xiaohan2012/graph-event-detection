7e227f49f02f5bb6ce6621acc05586b120b50b43
Han Xiao <han.xiao@cs.helsinki.fi>
2016-01-11 11:31:06 +0200

    experiment script added

---
+++ scripts/run_synthetic_data_experiment.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+methods=(variance lst greedy random)
+sampling_methods=(uniform out_degree)
+
+for method in "${methods[@]}"; do
+	for sampling_method in "${sampling_methods[@]}"; do
+		python gen_candidate_trees.py \
+			--method=${method} \
+			--root_sampling=${sampling_method} \
+			--seconds=8 --U=0.5 \
+			--dist=euclidean \
+			--cand_n=500 \
+			--res_dir=tmp/synthetic \
+			--lda_path=None \
+			--corpus_dict_path=None \
+			--interaction_path=data/synthetic/interactions.json \
+			--meta_graph_path_prefix=tmp/synthetic/meta-graph \
+			--given_topics
+	done
+done
