7405b5a82c2ec18dc9a7ff2456906a2e523d22f5
Han Xiao <han.xiao@cs.helsinki.fi>
2016-01-12 13:47:56 +0200

    option to build content.mm for islamic_lda.sh

--- scripts/islamic_lda.sh
+++ scripts/islamic_lda.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
-echo 'dumping messages'
-python dump_messages_from_interactions.py \
+if [ -z $1 ]; then
+    echo 'dumping messages'
+    python dump_messages_from_interactions.py \
 	--interactions_path data/islamic/interactions.json \
 	--output_path data/islamic/content.txt
 
-echo 'making .mm corpus'
-python lda/corpora.py -m data/islamic/content.txt \
+    echo 'making .mm corpus'
+    python lda/corpora.py -m data/islamic/content.txt \
 	-d data/islamic/dict.pkl \
 	-i data/islamic/id2token.pkl \
 	-o data/islamic/content.mm
+fi
 
 echo 'training model'
 time python lda/lda.py --n_topics 50 \
