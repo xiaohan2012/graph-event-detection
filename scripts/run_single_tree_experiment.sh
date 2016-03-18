881d4074dedd10f7d0cba56d611d046d5a1cbafa
Han Xiao <han.xiao@cs.helsinki.fi>
2016-03-18 10:35:44 +0200

    single tree experiment in one command

---
+++ scripts/run_single_tree_experiment.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+./scripts/synthetic_different_noise_fraction.sh data
+./scripts/synthetic_different_noise_fraction.sh gen
+./scripts/synthetic_different_noise_fraction.sh eval
+./scripts/synthetic_different_noise_fraction.sh viz
