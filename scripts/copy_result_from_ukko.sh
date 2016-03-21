48c3e7880e23c822b1b4c2674cc3c623f967693c
Han Xiao <han.xiao@cs.helsinki.fi>
2016-03-21 11:05:18 +0200

    script to copy result from ukko

---
+++ scripts/copy_result_from_ukko.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+if [ -z $1 ]; then
+	echo 'please give dataset name'
+	exit -1
+fi
+
+ds=$1
+
+rsync -v ukko140:code/lst/data/${ds}/model* data/${ds}
+rsync -v ukko140:code/lst/data/${ds}/dict* data/${ds}
+rsync -v ukko140:code/lst/data/${ds}/interactions* data/${ds}
+rsync -v ukko140:code/lst/tmp/${ds}/result* tmp/${ds}
+# scp ukko140:code/lst/data/${ds}/model* data/${ds}
+# scp ukko140:code/lst/data/${ds}/dict* data/${ds}
+# scp ukko140:code/lst/data/${ds}/interactions* data/${ds}
+# scp ukko140:code/lst/tmp/${ds}/result*.pkl tmp/${ds}
