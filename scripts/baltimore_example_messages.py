254779f134fcc978617ccf12092e3be2890e010d
Han Xiao <han.xiao@cs.helsinki.fi>
2016-03-29 14:39:57 +0300

    minor

--- scripts/baltimore_example_messages.py
+++ scripts/baltimore_example_messages.py
@@ -3,15 +3,18 @@ import pandas as pd
 import ujson as json
 
 d = json.load(open('/cs/home/hxiao/public_html/event_html/data/baltimore/event/meta_graph/result-greedy--U=15.0--dijkstra=False--timespan=1days----distance_weights={\"topics\":0.4,\"hashtag_bow\":0.4,\"bow\":0.2}--preprune_secs=1days--self_talking_penalty=0.0----cand_tree_percent=0.01--root_sampling=adaptive.json'))
+clean_msg = lambda t: t.replace('&amp;', '').replace('rt : ', '')
+
 ms = map(lambda r: r['body'], d[0]['nodes'])
-anger = [ms[0], ms[1], ms[3], ms[6], ms[7]]
+
+anger = [clean_msg(ms[i]) for i in [0, 1, 3, 6, 7]]
 
 ms = map(lambda r: r['body'], d[2]['nodes'])
-report = [ms[0], ms[1], ms[2], ms[4], ms[5]]
+report = [clean_msg(ms[i]) for i in [0, 1, 2, 4, 5]]
 
 df = pd.DataFrame(
     {'descriptive(event-3)': report, 'emotional(event-1)': anger},
     columns=['emotional(event-1)', 'descriptive(event-3)']
 )
-pd.set_option('display.max_colwidth', 60)
+pd.set_option('display.max_colwidth', 50)
 df.to_latex('tmp/baltimore/example_tweets.tex', encoding='utf8')
--- scripts/beefban_examples_messages.py
+++ scripts/beefban_examples_messages.py
@@ -4,7 +4,7 @@ import ujson as json
 
 d = json.load(open('/cs/home/hxiao/public_html/event_html/data/beefban/event/meta_graph/result-greedy--U=15.0--dijkstra=False--timespan=1days----distance_weights={\"topics\":0.4,\"hashtag_bow\":0.4,\"bow\":0.2}--preprune_secs=1days----cand_tree_percent=0.00193485972267--root_sampling=adaptive.json'))
 ms = map(lambda r: r['body'], d[0]['nodes'])
-clean_msg = lambda t: t.replace('&amp;', '').replace('#beefban', '').replace('rt : ', '')
+clean_msg = lambda t: t.replace('&amp;', '').replace('rt : ', '')
 support = [clean_msg(ms[i]) for i in [1, 8, 9, 10, 12]]
 
 ms = map(lambda r: r['body'], d[1]['nodes'])
@@ -14,5 +14,5 @@ df = pd.DataFrame(
     {'support(event-1)': support, 'against(event-2)': oppose},
     columns=['support(event-1)', 'against(event-2)']
 )
-pd.set_option('display.max_colwidth', 60)
+pd.set_option('display.max_colwidth', 50)
 df.to_latex('tmp/beefban/example_tweets.tex', encoding='utf8')
